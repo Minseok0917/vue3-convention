b494f2e3f62af508362f71ce77b86439130d59a6
Minseok0917 <nminseok0917@naver.com>
2022-07-17 01:51:32 +0900

    add resolve alias ( com, api, store, router, plugins )

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
-// https://vitejs.dev/config/
+const resolveAlias = Object.fromEntries(
+    Object.entries({
+        "@com": "./src/components",
+        "@api": "./src/api",
+        "@store": "./src/store",
+        "@router": "./src/router",
+        "@plugins": "./src/plugins",
+    }).map(([key, value]) => [key, path.resolve(__dirname, value)])
+);
+
 export default defineConfig({
-  plugins: [vue()]
-})
+    plugins: [vue()],
+    resolve: {
+        alias: resolveAlias,
+    },
+    server: {
+        host: true,
+    },
+});
